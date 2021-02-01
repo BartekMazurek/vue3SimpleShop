0a2c9a199366847a55a23b1dc07f540c3492c871
BartekMazurek <bartlomiejmazurek9@gmail.com>
2021-02-01 21:33:21 +0100

    Reorganized components in main view

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
 
-    <div class="container">
-        <div class="row">
+    <div class="container navigation-container">
+        <div class="row top-row">
             <div class="col-sm-12 col-md-6">
-                Vue SimpleShop
+                <h1 class="shop-name">simpleShop</h1>
             </div>
             <div class="col-sm-12 col-md-6">
                 <navigation></navigation>
@@ -11,12 +11,9 @@
         </div>
     </div>
 
-    <div class="container">
+    <div class="container content-container">
         <div class="row">
-            <div class="col-4">
-                <filters></filters>
-            </div>
-            <div class="col-8">
+            <div class="col-12">
                 <router-view/>
             </div>
         </div>
@@ -24,19 +21,36 @@
 
 </template>
 
-<style>
+<style scoped>
 
+.top-row {
+    margin-top: 10px;
+    border: 1px solid #ff5a00;
+}
+
+.top-row > div:first-child,
+.top-row > div:nth-child(2) {
+    padding: 30px;
+}
+
+.shop-name {
+    color: #ff5a00;
+    font-weight: 800;
+}
+
+.content-container .row > div:first-child {
+    margin-top: 20px;
+    padding: 0px;
+}
 
 </style>
 
 <script>
 
 import Navigation from "@/components/Navigation";
-import Filters from "@/components/Filters";
 
 export default {
     components: {
-        Filters,
         Navigation
     }
 }
