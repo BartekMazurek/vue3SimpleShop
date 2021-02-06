1d206eb2f093f70cd3cc2295669f20c98164566f
BartekMazurek <bartlomiejmazurek9@gmail.com>
2021-02-06 21:13:24 +0100

    Reorganized product list component

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,23 +1,29 @@
 <template>
     <div class="product-list">
-
         <template v-if="products.length">
             <div v-for="product in products" :key="product.name">
-                <p>Id: {{ product.id }} | Name: {{ product.name }} | Price {{ product.price }}</p>
+                <product-list-element
+                    :product-id="product.id"
+                    :product-name="product.name"
+                    :product-price="product.price">
+                </product-list-element>
             </div>
         </template>
-
         <template>
             <p>No products to show ... </p>
         </template>
-
     </div>
 </template>
 
 <script>
 
+import ProductListElement from "@/components/ProductListElement";
+
 export default {
     name: "ProductList",
+    components: {
+        ProductListElement
+    },
     props: {
         products: Array
     }
@@ -26,9 +32,11 @@ export default {
 </script>
 
 <style scoped>
+
 .product-list {
     margin-top: 20px;
     border: 1px solid #ff5a00;
     min-height: 100px;
 }
+
 </style>
