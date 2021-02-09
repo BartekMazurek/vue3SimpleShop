a1356996d32b199ad932cafe98c3fc8df39510b6
BartekMazurek <bartlomiejmazurek9@gmail.com>
2021-02-09 23:53:17 +0100

    Added styles into product list details component & show description

--- src/components/ProductListElementDetails.vue
+++ src/components/ProductListElementDetails.vue
@@ -1,17 +1,41 @@
 <template>
 
     <div class="product-list-element-details">
-        Some details about product with id {{ productIdToShowDetails }} ...
+        <div class="content-container">
+            <h5>Product description</h5>
+            {{ productDescription }}
+            {{ productId }}
+        </div>
+        <div class="close-window-row">
+            <button @click="closeProductDetailsWindow">CLOSE</button>
+        </div>
     </div>
 
 </template>
 
 <script>
 
+import ProductDetails from "@/service/ProductDetails/ProductDetails";
+
 export default {
     name: "ProductListElementDetails",
     props: {
-        productIdToShowDetails: Number
+        productId: {
+            type: Number,
+            required: true
+        }
+    },
+    data: () => ({
+        productDescription: ''
+    }),
+    created: () => {
+        let productDetailsService = new ProductDetails();
+        this.productDescription = productDetailsService.getProductDetailsById(this.productId);
+    },
+    methods: {
+        closeProductDetailsWindow() {
+            this.$emit('closeProductDetailsWindow', false);
+        }
     }
 }
 
@@ -20,17 +44,34 @@ export default {
 <style scoped>
 
 .product-list-element-details {
-    width: 600px;
-    height: 400px;
+    width: 800px;
+    height: 600px;
     position: absolute;
-    top: 10%;
-    left: 15%;
+    top: 0px;
     margin: 0 auto;
     background-color: #FFFF;
     color: gray;
     border: 1px solid lightgrey;
     -webkit-box-shadow: 2px 2px 10px 0 rgba(0, 0, 0, 0.3);
     box-shadow: 2px 2px 10px 0 rgba(0, 0, 0, 0.3);
+}
+
+.product-list-element-details .content-container {
+    margin: 30px;
+    height: 400px;
+    max-height: 400px;
+    overflow-x: auto;
+}
+
+.product-list-element-details button {
+    background-color: #ff5a00;
+    color: #FFFF;
+    border: 1px solid #ff5a00;
+    height: 30px;
+    margin-left: 10px;
+    padding: 0px 10px 0px 10px;
+    font-size: 14px;
+    border-radius: 0px;
 }
 
 </style>
