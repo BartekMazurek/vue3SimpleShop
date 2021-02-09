02e1ba8bef396e0dd6f7087611d26c954ea23286
BartekMazurek <bartlomiejmazurek9@gmail.com>
2021-02-10 00:13:28 +0100

    Modified product details service

--- src/service/ProductDetails/ProductDetails.js
+++ src/service/ProductDetails/ProductDetails.js
@@ -1,7 +1,8 @@
 export default class ProductDetails {
 
   getProductDetailsById(productId) {
-    return 'Description of product with id ' + productId + 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n' +
+    console.log(productId);
+    return 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n' +
       '            industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and\n' +
       '            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into\n' +
       '            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of\n' +
